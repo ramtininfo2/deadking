f1cba05316a2b71287784a13efef390e61f6a890
ramtininfo2 <r.harandi.rh.v2@gmail.com>
2016-04-08 17:07:17 +0430

    Create antimedia.lua

---
+++ plugins/antimedia.lua
@@ -0,0 +1,32 @@
+--An empty table for solving multiple kicking problem(thanks to @topkecleon )
+kicktable = {}
+
+
+local function run(msg, matches)
+    if is_momod(msg) then
+        return
+    end
+    local data = load_data(_config.moderation.data)
+    if data[tostring(msg.to.id)] then
+        if data[tostring(msg.to.id)]['settings'] then
+            if data[tostring(msg.to.id)]['settings']['lock_media'] then
+                lock_media = data[tostring(msg.to.id)]['settings']['lock_media']
+            end
+        end
+    end
+    local chat = get_receiver(msg)
+    local user = "user#id"..msg.from.id
+    if lock_media == "yes" then
+        send_large_msg(chat, 'share media is not allowed here!!')
+        chat_del_user(chat, user, ok_cb, true)
+    end
+end
+ 
+return {
+  patterns = {
+  "%[(photo)%]",
+  "%[(audio)%]",
+  "%[(video)%]" 
+  },
+  run = run
+}

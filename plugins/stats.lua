eacf48cf3c08d636098cac19e915c580131c09c9
ramtininfo2 <r.harandi.rh.v2@gmail.com>
2016-04-09 12:47:28 +0430

    fixed

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -91,10 +91,9 @@ local function bot_stats()
   return text
 end
 local function run(msg, matches)
-  if matches[1]:lower() == 'teleseed' then -- Put everything you like :)
+  if matches[1]:lower() == 'deadking' then -- Put everything you like :)
     local about = _config.about_text
     local name = user_print_name(msg.from)
-    savelog(msg.to.id, name.." ["..msg.from.id.."] used /teleseed ")
     return about
   end 
   if matches[1]:lower() == "statslist" then
@@ -120,7 +119,7 @@ local function run(msg, matches)
         return
       end
     end
-    if matches[2] == "teleseed" then -- Put everything you like :)
+    if matches[2] == "deadking" then -- Put everything you like :)
       if not is_admin(msg) then
         return "For admins only !"
       else
@@ -141,8 +140,8 @@ return {
     "^[!/]([Ss]tats)$",
     "^[!/]([Ss]tatslist)$",
     "^[!/]([Ss]tats) (group) (%d+)",
-    "^[!/]([Ss]tats) (teleseed)",-- Put everything you like :)
-		"^[!/]([Tt]eleseed)"-- Put everything you like :)
+    "^[!/]([Ss]tats) (deadking)",
+    "^[!/]([Dd]eadking)"
     }, 
   run = run
 }

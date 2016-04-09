e64e742a8f99459c7483843a005d16347c36959f
ramtininfo2 <r.harandi.rh.v2@gmail.com>
2016-04-09 12:42:36 +0430

    fixed

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -1,8 +1,7 @@
 do
 -- Will leave the group if be added
 local function run(msg, matches)
-local bot_id = our_id -- your bot id
-   -- like local bot_id = 1234567
+local bot_id = 1234567
     if matches[1] == 'leave' and is_admin(msg) then
        chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
@@ -15,6 +14,7 @@ end
 return {
   patterns = {
     "^[!/](leave)$",
+    "^(leave)$",
     "^!!tgservice (.+)$",
   },
   run = run

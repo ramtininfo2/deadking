d1823541c50fec34e61c8f67c7cee0763f9dd874
ramtininfo2 <r.harandi.rh.v2@gmail.com>
2016-04-12 11:44:21 +0430

    fixed

--- plugins/antijoin.lua
+++ plugins/antijoin.lua
@@ -1,7 +1,4 @@
 local function run (msg, matches)
-    if is_momod(msg) then
-        return nil
-    end
 local data = load_data(_config.moderation.data)
    if matches[1] == 'chat_add_user_link' then
         local user_id = msg.from.id
@@ -9,12 +6,12 @@ local data = load_data(_config.moderation.data)
         if data[tostring(msg.to.id)]['settings'] then
         if data[tostring(msg.to.id)]['settings']['lock_join'] == 'yes' then
       kick_user(user_id, msg.to.id)
-      send_large_msg(get_receiver(msg), "Member add is lock")
-                    end
-                end
-            end   
-        end
+     send_large_msg(get_receiver(msg), "member add is lock!")
     end
+   end
+end   
+ end
+ end
 return {
   patterns = {
     "^!!tgservice (chat_add_user_link)$"
